89e2510c780e89decd1c4be6ae97ca7f337d1fc0
bloxboy <frederic.bloxboy@gmail.com>
2021-10-29 16:59:04 +0200

    [CHG] Images and i18n

--- src/_data/i18n/index.js
+++ src/_data/i18n/index.js
@@ -21,18 +21,182 @@ module.exports = {
 
   // section 1
   title1: {
-    'en': 'no title',
+    'en': 'Collaboratively Building Innovative Digital Learning Services',
     'fr': 'Co-construire des services numériques innovants pour l\'éducation\n'
   },
   undertitle1: {
-    'en': 'no title',
+    'en': 'We federate a network of universities, research labs, government agencies, edtech\n' +
+        'companies and individuals dedicated to building great and sovereign digital services\n' +
+        'for education.',
     'fr': 'Nous fédérons un réseau d\'universités, d\'écoles, d\'instituts de recherche, d\'agences gouvernementales, d\'entreprises edtech et de contributeurs qui se consacrent à la construction de services numériques souverains pour l\'éducation.'
   },
-  // Navigation
-  homepage: {
-    'en': 'Homepage',
-    'fr': 'Accueil'
+
+  // section tools
+
+  Tools : {
+    'en': 'Tools',
+    'fr': 'Outils',
   },
+  Tools_title: {
+    'en': 'Handcrafting opensource learning tools',
+    'fr': 'Développement d\'outils d\'apprentissage libres et ouverts'
+  },
+  Tools_Catalogue: {
+    'en': 'Catalog & Marketplace',
+    'fr': 'Catalogue et place de marché'
+  },
+  Tools_System: {
+    'en': 'Learning Management System / LMS',
+    'fr': 'Système de gestion d\'apprentissage'
+  },
+  Tools_Gestion: {
+    'en': 'Collaboration Tools',
+    'fr': 'Gestion de communauté et communication'
+  },
+  Tools_Services: {
+    'en': 'Media Services',
+    'fr': 'Services multimédias'
+  },
+  Tools_Learning: {
+    'en': 'Learning Analytics',
+    'fr': 'Learning Analytics'
+  },
+
+  // section about
+
+  about_ethiques: {
+    'en': 'Ethical',
+    'fr': 'Éthique'
+  },
+  about_title: {
+    'en': 'An ethical and respectful posture towards regulations, rights and sovereignty',
+    'fr': 'Une posture éthique et respectueuse de la réglementation, des droits des personnes et de la souveraineté'
+  },
+  about_software: {
+    'en': 'Open source',
+    'fr': 'Logiciels libres et ouverts'
+  },
+  about_RGPD: {
+    'en': 'GPDR',
+    'fr': 'RGPD'
+  },
+  about_Access: {
+    'en': 'Accessibility',
+    'fr': 'Accessibilité'
+  },
+  about_Facil: {
+    'en': 'Digital Sovereignty Facilitator',
+    'fr': 'Facilitateur de souveraineté numérique'
+  },
+
+  //Numbers
+
+  Numbers_subtitle: {
+    'en': 'no title',
+    'fr': 'Ressources et communauté'
+  },
+  Numbers_title: {
+    'en': 'France Université Numérique in figures',
+    'fr': 'France Université Numérique en quelques chiffres'
+  },
+  Numbers_views: {
+    'en': 'videos views per month',
+    'fr': 'mille vues de vidéos par mois'
+  },
+  Numbers_sessions: {
+    'en': 'course sessions',
+    'fr': 'sessions de cours'
+  },
+  Numbers_instit: {
+    'en': 'Public institutions and partner organizations',
+    'fr': 'institutions publiques et organisations partenaires'
+  },
+  Numbers_projects: {
+    'en': 'active open source projects',
+    'fr': 'projets actifs de développement libres et ouverts'
+  },
+  Numbers_events: {
+    'en': 'learning events/day',
+    'fr': 'million d\'événements d\'apprentissage par jour'
+  },
+  Numbers_people: {
+    'en': 'people in the Community',
+    'fr': 'personnes au sein de la communauté'
+  },
+  Numbers_learner: {
+    'en': 'million learners',
+    'fr': 'millions d\'apprenants'
+  },
+  Numbers_visit: {
+    'en': 'annual visits ',
+    'fr': 'millions de visites annuelles'
+  },
+
+  /// APPS
+
+  App_subtitle: {
+    'en': 'Platforms',
+    'fr': 'Plateformes'
+  },
+  App_title: {
+    'en': 'Tailored Learning Platforms & Services ',
+    'fr': 'Des plateformes et services d apprentissage sur mesure'
+  },
+  App_mooc: {
+    'en': 'FUN MOOC',
+    'fr': 'FUN MOOC'
+  },
+  App_moocText: {
+    'en': 'Online courses to discover, learn, progress and succeed',
+    'fr': 'Des cours en ligne pour découvrir, apprendre, progresser et réussir'
+  },
+  App_campus: {
+    'en': 'FUN CAMPUS',
+    'fr': 'FUN CAMPUS'
+  },
+  App_campusText: {
+    'en': 'notext',
+    'fr': 'Des formations pour enrichir les cursus'
+  },
+  App_corp: {
+    'en': 'FUN Corporate',
+    'fr': 'FUN Corporate'
+  },
+  App_corpText: {
+    'en': 'notext',
+    'fr': 'Informez-vous sur les évolutions du bâtiment durable !'
+  },
+  App_meet: {
+    'en': 'Meeting.education',
+    'fr': 'Meeting.education'
+  },
+  App_meetText: {
+    'en': 'notext',
+    'fr': 'Des cours en ligne pour découvrir, apprendre, progresser et réussir'
+  },
+
+  //Contact
+
+  Contact: {
+    'en': 'Contact us',
+    'fr': 'Contactez-nous'
+  },
+  Contact_text: {
+    'en': 'text english contact',
+    'fr': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam 🎉'
+  },
+  Contact_button: {
+    'en': 'Contact Form',
+    'fr': 'Formulaire pour être recontacté'
+  },
+
+  //
+  Testimonials: {
+    'en': 'Testimonials',
+    'fr': 'Témoignages'
+  },
+
+  //
   posts: {
     'en': 'Posts',
     'fr': 'Publicacions'
